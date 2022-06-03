5cad5e92e781c16450acc633e649bec342aaf954
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-06-03 15:06:28 -0300

    add routes file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import express from "express";
+import express from "express"
+import routes from "./routes"
 
-const PORT = process.env.PORT || 2222;
-const app = express();
+const PORT = process.env.PORT || 2222
+const app = express()
 
-app.listen(PORT, () => console.log(`server is runnig at port ${PORT}`));
+app.use(routes)
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`server is runnig at port ${PORT}`))
