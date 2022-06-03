4f5db17dd470a68e330c1f7218075194fcc1bba1
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-06-03 15:30:31 -0300

    add message-model

---
+++ src/models/message-model.ts
@@ -0,0 +1,32 @@
+import db from "../config/db-config"
+
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+} from "sequelize"
+
+interface Message
+  extends Model<InferAttributes<Message>, InferCreationAttributes<Message>> {
+  id: string
+  template_name: string
+  position: number
+}
+const MessageModel = db.define<Message>("message_flow", {
+  id: {
+    type: DataTypes.UUIDV4,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  template_name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  position: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+})
+
+export default MessageModel
