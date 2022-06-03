a43ec0eb5f25cee3ae57bc131ffe6a53e34ef769
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-06-03 15:16:09 -0300

    add sequelize orm

---
+++ redme.md
@@ -0,0 +1,9 @@
+<h1 align="center">Desafio Back-end Precato</h1>
+
+## Tecnologias utilizadas
+
+- Express
+- Nodejs
+- Typescript
+- Sequelize
+- Sqlite
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import routes from "./routes"
+import db from "./config/db-config"
 
 const PORT = process.env.PORT || 2222
 const app = express()
 
 app.use(routes)
 app.use(express.json())
+db.sync()
+  .then(() => console.log("databse sycronnized"))
+  .catch(e => console.log("database error " + e))
 
 app.listen(PORT, () => console.log(`server is runnig at port ${PORT}`))
