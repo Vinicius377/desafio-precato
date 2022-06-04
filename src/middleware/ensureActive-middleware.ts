10dfb0ac00bcd3ca54662f18398ee69b40956b0d
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-06-03 23:20:25 -0300

    add ensure active middleware

---
+++ src/middleware/ensureActive-middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express"
+import SubscriptionModel from "../models/subscription-model"
+
+async function EnsureActive(req: Request, res: Response, next: NextFunction) {
+  const { id } = req.body
+  if (!id) {
+    res.status(400).json({ error: "needed id" })
+    return
+  }
+  const data = await SubscriptionModel.findOne({ where: { id } })
+  if (!data?.active) {
+    res.status(400).json({ message: "user is not subscribe" })
+  } else {
+    res.locals.lastMessage = data.last_message
+    next()
+  }
+}
+
+export default EnsureActive
